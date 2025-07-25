4987959b8149c0d9fcb1b717e966b0f4c3c47d5c
Your Name <you@example.com>
2025-07-25 10:50:57 +0530

    54.0.1

---
+++ lucide/luicons.js
@@ -0,0 +1,27 @@
+function preloadImage(src) {
+    const img = new Image();
+    img.src = src;
+}
+
+function loadLucideIcons() {
+    const icons = document.querySelectorAll(".lu-icon");
+    icons.forEach((icon) => {
+        const iconClasses = new Set(icon.classList);
+        const iconName = Array.from(iconClasses).find((cl) => cl.startsWith("luicn-"));
+
+        if (iconName) {
+            const iconNameFormatted = iconName.split("luicn-")[1];
+            const imagePath = `https://cdn.jsdelivr.net/gh/jovanfever/onyx@54.0.1/lucide/icons/${iconNameFormatted}.svg`;
+
+            // Preload the image
+            preloadImage(imagePath);
+
+            // Set the background image
+            icon.style.backgroundImage = `url('${imagePath}')`;
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadLucideIcons();
+});
